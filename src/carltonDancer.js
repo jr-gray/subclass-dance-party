10a09a0ddfe9c07f9cf441a39680129db794d01d
Hack Reactor Student <student@hackreactor.com>
2017-07-03 17:09:12 -0700

    Dance floor and 2 new dancers added

---
+++ src/carltonDancer.js
@@ -0,0 +1,47 @@
+// var makeCarltonDancer = function(top, left, timeBetweenSteps) {
+//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+
+//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+//   // so we must keep a copy of the old version of this function
+
+//   var oldStep = blinkyDancer.step;
+
+  // blinkyDancer.step = function() {
+  //   // call the old version of step at the beginning of any call to this new version of step
+  //   oldStep();
+  //   // toggle() is a jQuery method to show/hide the <span> tag.
+  //   // See http://api.jquery.com/category/effects/ for this and
+  //   // other effects you can use on a jQuery-wrapped html tag.
+  //   blinkyDancer.$node.toggle();
+  // };
+
+//   return blinkyDancer;
+// };
+
+
+var makeCarltonDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('carltondancer');
+  this.$node.removeClass('dancer');
+  this.$node.html('<img class="carltondancer" src="http://imgur.com/vDNZM1D.gif">');
+  // var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  //this.oldStep = this.step;
+};
+
+makeCarltonDancer.prototype = Object.create(makeDancer.prototype);
+makeCarltonDancer.prototype.constructor = makeCarltonDancer;
+
+makeCarltonDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  makeDancer.prototype.step.call(this);
+  
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  //this.$node.toggle();
+};
+
---
+++ src/gangnamDancer.js
@@ -0,0 +1,47 @@
+// var makeGangNamDancer = function(top, left, timeBetweenSteps) {
+//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+
+//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+//   // so we must keep a copy of the old version of this function
+
+//   var oldStep = blinkyDancer.step;
+
+  // blinkyDancer.step = function() {
+  //   // call the old version of step at the beginning of any call to this new version of step
+  //   oldStep();
+  //   // toggle() is a jQuery method to show/hide the <span> tag.
+  //   // See http://api.jquery.com/category/effects/ for this and
+  //   // other effects you can use on a jQuery-wrapped html tag.
+  //   blinkyDancer.$node.toggle();
+  // };
+
+//   return blinkyDancer;
+// };
+
+
+var makeGangNamDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('gangnamdancer');
+  this.$node.removeClass('dancer');
+  this.$node.html('<img class="gangnamdancer" src="http://gifdanceparty.giphy.com/assets/dancers/cowboy.gif">');
+  // var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  //this.oldStep = this.step;
+};
+
+makeGangNamDancer.prototype = Object.create(makeDancer.prototype);
+makeGangNamDancer.prototype.constructor = makeGangNamDancer;
+
+makeGangNamDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  makeDancer.prototype.step.call(this);
+  
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  //this.$node.toggle();
+};
+
